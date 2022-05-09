e4e6a13a66f908417b2f2fc4109daeac5371df1b
Alan <mvpi3.tw@gmail.com>
2022-05-09 15:17:51 +0800

    Add UIImage+NYXRotating again

---
+++ AppFactory/Core/Class/Helper/ImagePicker/UIImage+NYXRotating.h
@@ -0,0 +1,27 @@
+//
+//  UIImage+Rotation.h
+//  NYXImagesKit
+//
+//  Created by @Nyx0uf on 02/05/11.
+//  Copyright 2012 Nyx0uf. All rights reserved.
+//  www.cocoaintheshell.com
+//
+
+
+#import "NYXImagesHelper.h"
+
+@interface UIImage (NYXRotating)
+
+-(UIImage*)rotateInRadians:(float)radians;
+
+-(UIImage*)rotateInDegrees:(float)degrees;
+
+-(UIImage*)rotateImagePixelsInRadians:(float)radians;
+
+-(UIImage*)rotateImagePixelsInDegrees:(float)degrees;
+
+-(UIImage*)verticalFlip;
+
+-(UIImage*)horizontalFlip;
+
+@end
